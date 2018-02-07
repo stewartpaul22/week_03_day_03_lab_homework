add6afa29f597c79155e2230da72770d4b8e928d
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-07 17:16:13 +0000

    Add an all() method to the Album class

--- models/album.rb
+++ models/album.rb
@@ -14,14 +14,17 @@ class Album
     @artist_id = options['artist_id'].to_i
   end
 
-  # Create and Save Albums
   def save()
     sql = "INSERT INTO albums (title, genre, artist_id) VALUES ($1, $2, $3) RETURNING id"
     values = [@title, @genre, @artist_id]
     @id = SqlRunner.run(sql, values)[0]['id'].to_i
   end
 
-  # List All Artists/Albums
+  def self.all()
+    sql = "SELECT * FROM albums"
+    albums = SqlRunner.run(sql)
+    return albums.map{|album| Album.new(album)}
+  end
 
   # List all the albums they have by an artist
 
